fd9bda3064ed3a4cc8a6b70b20ceff7a8e55ccc4
程安康 <952472388@qq.com>
2018-09-05 14:53:00 +0800

    枚举类使用

--- _str_.py
+++ _str_.py
@@ -28,3 +28,46 @@ class Fib(object):
 
 for n in Fib():
     print(n)
+
+# __getitem__类 类似list一样能够取出下标元素
+
+class Fib1 (object):
+    def __getitem__(self,n):
+        a,b = 1,1
+        for x in range(n):
+            a,b = b,a+b
+        return a
+
+f = Fib1()
+print(f[20])
+# print(list(range(5)))
+
+# __getattr__ 获取属性
+class Getattr(object):
+    def __init__(self):
+        self.name = "kk"
+
+    def __getattr__(self,attr):
+        if attr == 'score':
+            # return 99
+            return lambda :99
+
+b = Getattr()
+# print(b.score
+print(b.score())
+
+# 试试
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+    
+    def __getattr__(self, path):
+        return Chain("%s/%s" % (self._path, path))
+
+    def __str__(self):
+        return self._path
+
+    __repr__ = __str__
+
+# 属性链式调用
+print(Chain().sd.sdd.sddd.sdddd)
